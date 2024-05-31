b5d997babc664e1926b8f3e3ddcdc4ef48f338f7
Arman Rasouli <paplix.dev@gmail.com>
2024-05-31 19:27:44 +0800

    Name Change

--- datetime.py
+++
@@ -1,9 +0,0 @@
-from DateTime import DateTime
-
-e = DateTime('MST')
-
-
-print(e)
-
-
-print("Hello, West Coast!")
---
+++ py_test.py
@@ -0,0 +1,9 @@
+from DateTime import DateTime
+
+e = DateTime('MST')
+
+
+print(e)
+
+
+print("Hello, West Coast!")
